d8fa3289ff1786b80cdfd743726c008c12b5ddf1
Bora Yıldırım <borayldrm@hotmail.com>
2025-08-09 03:48:23 +0300

    refactor(auth): Finalize Splash login-gating cleanup in ViewModel

--- app/src/main/java/com/borayildirim/foodiehub/presentation/viewmodels/SplashViewModel.kt
+++ app/src/main/java/com/borayildirim/foodiehub/presentation/viewmodels/SplashViewModel.kt
@@ -2,7 +2,6 @@ package com.borayildirim.foodiehub.presentation.viewmodels
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.borayildirim.foodiehub.domain.usecase.CheckUserLoginStatusUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -11,29 +10,16 @@ import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class SplashViewModel @Inject constructor (
-    private val checkUserLoginStatusUseCase: CheckUserLoginStatusUseCase
-): ViewModel() {
+class SplashViewModel @Inject constructor (): ViewModel() {
 
-    private val _uiState = MutableStateFlow(SplashState())
-    val uiState = _uiState.asStateFlow()
-
-    data class SplashState(
-        val isLoading: Boolean = true,
-        val shouldNavigateToHome: Boolean = false,
-        val shouldNavigateToAuth: Boolean = false
-    )
+    private val _shouldNavigate = MutableStateFlow(false)
+    val shouldNavigate = _shouldNavigate.asStateFlow()
 
     fun startSplash() {
         viewModelScope.launch {
-            val isLoggedIn = checkUserLoginStatusUseCase.checkLoginStatus()
             delay(2000)
+            _shouldNavigate.value = true
 
-            _uiState.value = _uiState.value.copy(
-                isLoading = false,
-                shouldNavigateToHome = isLoggedIn,
-                shouldNavigateToAuth = !isLoggedIn
-            )
         }
     }
 }
